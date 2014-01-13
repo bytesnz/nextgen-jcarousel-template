b73ddd8f5e21b606d2fa276d951a681743add085
Jack Farley <jack@theweldstudio.com>
2014-01-13 18:11:15 +1300

    Created dump script for building zip file of plugin

---
+++ build.sh
@@ -0,0 +1,2 @@
+cd ..
+zip nextgen-jcarousel-template/nextgen-jcarousel-template.zip nextgen-jcarousel-template/{js/jcarousel-0.3.0/{jquery.jcarousel.min.js,jquery.jcarousel-autoscroll.min.js},template-jcarousel.php,LICENSE,nextgen-jcarousel-template.php,readme.txt,css/style.css}
